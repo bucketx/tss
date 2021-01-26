6e0750857cc7adff773add84f04f8d96174e7337
Leung Yau Ming <leungyauming2005@gmail.com>
2021-01-27 00:01:36 +0800

    feat: add basic functionality
    image conversion functionality is added
    image scaling functionality is added
    
    users can create acceptable png images for Telegram to create stickers

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
+use std::env;
+
+use image::imageops::FilterType;
+use image::io::Reader as ImageReader;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() >= 2 {
+        for arg in &args[1..] {
+            let input_image_reader = match ImageReader::open(arg) {
+                Ok(reader) => reader,
+                Err(_) => {
+                    println!("ERROR: Failed to open image '{}' . . . SKIPPED", arg);
+                    continue;
+                }
+            };
+
+            let input_image = match input_image_reader.decode() {
+                Ok(image) => image,
+                Err(_) => {
+                    println!("ERROR: Failed to decode image '{}' . . . SKIPPED", arg);
+                    continue;
+                }
+            };
+
+            let output_image_filename = format!("{}{}", arg, ".png");
+            match input_image
+                .resize(512, 512, FilterType::Triangle)
+                .save(&output_image_filename)
+            {
+                Ok(_) => {}
+                Err(_) => {
+                    println!(
+                        "ERROR: Failed to write image to '{}' file",
+                        &output_image_filename
+                    );
+                    continue;
+                }
+            }
+        }
+    } else {
+        println!("Usage: {} <image> ...", &args[0])
+    }
 }
