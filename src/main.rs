bfbc20895dbdb2efae3077327d308ca2493dc738
Leung Yau Ming <leungyauming2005@gmail.com>
2021-01-28 08:53:24 +0800

    feat!: replace old plain arguents parse with cli flags
    BREAKING CHANGE: cli flags are used for setting input images and output directory now

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,66 @@
-use std::env;
-
 use image::imageops::FilterType;
 use image::io::Reader as ImageReader;
 
+use clap::clap_app;
+
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let tss = clap_app!(tss =>
+        (version: "0.0.1-dev")
+        (author: "BucketX")
+        (about: "Telegram Sticker Scaler")
+        (@arg input: -i --input +required +takes_value ... "Set input images")
+        (@arg output: -o --output +takes_value "Set output directory")
+    )
+    .get_matches();
 
-    if args.len() >= 2 {
-        for arg in &args[1..] {
-            let input_image_reader = match ImageReader::open(arg) {
-                Ok(reader) => reader,
-                Err(_) => {
-                    println!("ERROR: Failed to open image '{}' . . . SKIPPED", arg);
-                    continue;
-                }
-            };
+    let input_images: Vec<&str> = tss
+        .values_of("input")
+        .expect("Input images are required.")
+        .collect();
+    let mut output_directory = String::from(tss.value_of("output").unwrap_or("").trim());
 
-            let input_image = match input_image_reader.decode() {
-                Ok(image) => image,
-                Err(_) => {
-                    println!("ERROR: Failed to decode image '{}' . . . SKIPPED", arg);
-                    continue;
-                }
-            };
+    if output_directory.len() != 0 && output_directory.ends_with("/") {
+        output_directory.push('/');
+    }
 
-            let output_image_filename = format!("{}{}", arg, ".png");
-            match input_image
-                .resize(512, 512, FilterType::Triangle)
-                .save(&output_image_filename)
-            {
-                Ok(_) => {}
-                Err(_) => {
-                    println!(
-                        "ERROR: Failed to write image to '{}' file",
-                        &output_image_filename
-                    );
-                    continue;
-                }
+    for image in input_images {
+        let input_image_reader = match ImageReader::open(image) {
+            Ok(reader) => reader,
+            Err(err) => {
+                println!(
+                    "ERROR: Failed to open image '{}' -> {} . . . SKIPPED",
+                    image, err
+                );
+                continue;
+            }
+        };
+
+        let input_image = match input_image_reader.decode() {
+            Ok(image) => image,
+            Err(err) => {
+                println!(
+                    "ERROR: Failed to decode image '{}' -> {} . . . SKIPPED",
+                    image, err
+                );
+                continue;
+            }
+        };
+
+        let output_image_filename = format!("{}{}{}", output_directory, image, ".png");
+        match input_image
+            .resize(512, 512, FilterType::Triangle)
+            .save(&output_image_filename)
+        {
+            Ok(_) => {
+                println!("INFO: Created new image '{}'", &output_image_filename);
+            }
+            Err(_) => {
+                println!(
+                    "ERROR: Failed to write image to '{}' file",
+                    &output_image_filename
+                );
+                continue;
             }
         }
-    } else {
-        println!("Usage: {} <image> ...", &args[0])
     }
 }
